835b2d776338a29e24930b47bde225f6d76b1cee
Noriel <flores1noriel@gmail.com>
2023-03-31 15:56:54 -0700

    Passed all tests on set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  var keys = Object.keys(this._storage);
+  this._storage[keys.length] = item;
 };
 
 setPrototype.contains = function(item) {
+  var storage = this._storage;
+  for (var key in storage) {
+    if (storage[key] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  var keys = Object.keys(this._storage);
+  var storage = this._storage;
+  for (var key in storage) {
+    if (storage[key] === item) {
+      delete storage[key];
+    }
+  }
 };
 
 /*
