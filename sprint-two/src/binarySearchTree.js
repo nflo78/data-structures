b854d9667a3c34a9687b974a45f8e3c2d1b92c62
Noriel <flores1noriel@gmail.com>
2023-04-01 14:02:30 -0700

    Passed all tests on binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,62 @@
 var BinarySearchTree = function(value) {
+  var storage = {value: value};
+  _.extend(storage, binaryMethods)
+  return storage
 };
 
+var binaryMethods = {};
 
+binaryMethods.insert = function (value) {
+  var recurse = function (branch) {
+    if (value > branch.value && branch.right === undefined) {
+      branch.right = {}
+      branch.right.value = value
+    }
+    if (value < branch.value && branch.left === undefined) {
+      branch.left = {}
+      branch.left.value = value
+    }
+    if (value > branch.value && branch.right) {
+      recurse(branch.right)
+    }
+    if (value < branch.value && branch.left) {
+      recurse(branch.left)
+    }
+  }
+  recurse(this)
+}
+
+binaryMethods.contains = function (target) {
+  var array = []
+  var recurse = function (branch) {
+    array.push(branch.value)
+    if (branch.right) {
+      recurse(branch.right)
+    }
+    if (branch.left) {
+      recurse(branch.left)
+    }
+  }
+  recurse(this)
+  if (array.indexOf(target) !== -1) {
+    return true
+  } else {
+    return false;
+  }
+}
+
+binaryMethods.depthFirstLog = function (cb) {
+  var recurse = function (branch) {
+    cb(branch.value)
+    if (branch.left) {
+      recurse(branch.left)
+    }
+    if (branch.right) {
+      recurse(branch.right)
+    }
+  }
+  recurse(this)
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
